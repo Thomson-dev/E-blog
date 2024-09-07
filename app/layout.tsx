e72ff2c8243c1504d921f39f28f85a4970dc16b3
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-09-07 23:13:56 +0100

    bug fix2

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* @ts-ignore */}
         <ReduxProvider store={store}>{children}</ReduxProvider>
       </body>
     </html>
